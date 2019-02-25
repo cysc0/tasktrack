71a9e53919ea3e669b9ea2d3af768e08862d40b7
Chris Gast <cisco@mothership.local>
2019-02-25 14:49:12 -0500

    init

---
+++ lib/tasktrack.ex
@@ -0,0 +1,18 @@
+defmodule Tasktrack do
+  @moduledoc """
+  Documentation for Tasktrack.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tasktrack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
