c0764b8ba41ba90a83ec36d3702e825ec314cf32
Chris Gast <cisco@mothership.local>
2019-02-26 17:28:40 -0500

    oopsie

--- lib/tasktrack_web/router.ex
+++ lib/tasktrack_web/router.ex
@@ -17,7 +17,7 @@ defmodule TasktrackWeb.Router do
       pipe_through :browser # Use the default browser stack
   
       get "/", PageController, :index
-      resources "/products", ProductController
+      resources "/tasks", TaskController
     end
   
     # Other scopes may use custom stacks.
